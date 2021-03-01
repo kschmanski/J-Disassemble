de966ba3c19e98c4f6f07b2a1c2c312549213df2
Kaz Schmanski <kschmanski1@gmail.com>
2021-02-28 20:52:28 -0600

    create construct hash map function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,56 +8,20 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.jsoup.nodes.Element;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello!");
 
+        Map<String, String> map = constructHashMap();
 
-        // write your code here
-
-//        try {
-//            //String x = Main.binaryFileToHexString("/Users/kris/Desktop/Personal/DePaul/SE526/disassembler/invaders.h");
-//            //String x = Main.binaryFileToHexString("/Users/kris/Desktop/Personal/DePaul/SE526/disassembler/main");
-//            //System.out.println(" X is: " + x + "\n");
-//
-//
-//
-//        } catch (Exception e) {
-//
-//            System.out.println("something bad happened: " + e);
-//
-//        }
-
-        String html = "<html><head><title>First parse</title></head>"
-                + "<body><p>Parsed HTML into a doc.</p></body></html>";
-        //Document doc = Jsoup.parse(html);
-        ArrayList<String> downServers = new ArrayList<String>();
-        try {
-            Document doc = Jsoup.connect("http://sparksandflames.com/files/x86InstructionChart.html").get();
-
-            Element table = doc.select("table").get(0); //select the first table.
-            Elements rows = table.select("tr");
-
-
-            for (int i = 0; i < rows.size(); i++) {
-                Element row = rows.get(i);
-                Elements cols = row.select("td");
-
-                for (int j = 0; j < cols.size(); j++) {
-                    System.out.println(cols.get(j));
-                }
-
-
-
-            }
-
-        } catch (Exception e) {
-
-        }
+        System.out.println(Arrays.asList(map)); // method 1
 
     }
+    
     public static String binaryFileToHexString(final String path)
             throws FileNotFoundException, IOException
     {
@@ -83,8 +47,42 @@ public class Main {
         return sb.toString();
     }
 
-    private static void print(String msg, Object... args) {
-        System.out.println(String.format(msg, args));
+    private static Map<String, String> constructHashMap() {
+
+        Map map = new HashMap<String, String>();
+
+
+        try {
+            Document doc = Jsoup.connect("http://sparksandflames.com/files/x86InstructionChart.html").get();
+
+            Element table = doc.select("table").get(0); //select the first table.
+            Elements rows = table.select("tr");
+
+
+            for (int i = 0; i < rows.size(); i++) {
+                Element row = rows.get(i);
+                Elements cols = row.select("td");
+
+                for (int j = 0; j < cols.size(); j++) {
+                    //System.out.println(cols.get(j));
+
+                    Elements opCode = cols.get(j).select("i");
+                    Elements opCodeName = cols.get(j).select("b");
+
+                    String opCodetext = opCode.text();
+                    String opCodeNameText = opCodeName.text();
+
+                    map.put(opCodetext, opCodeNameText);
+                }
+
+            }
+
+        } catch (Exception e) {
+
+        }
+
+        return map;
+
     }
 
 }
