9eaa47f987b0443aa1e176d38c53513cd14b334b
Kaz Schmanski <kschmanski1@gmail.com>
2021-03-06 14:11:40 -0600

    code cleanup

--- src/main/java/Helper.java
+++ src/main/java/Helper.java
@@ -4,83 +4,47 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Helper {
 
-    //TODO - rework this function, docblock, change name, variable names
-    public static String binaryFileToHexString(final String path)
-            throws FileNotFoundException, IOException
-    {
-        final int bufferSize = 512;
-        final byte[] buffer = new byte[bufferSize];
-        final StringBuilder sb = new StringBuilder();
-
-        // open the file
-        FileInputStream stream = new FileInputStream(path);
-        int bytesRead;
-
-        // read a block
-        while ((bytesRead = stream.read(buffer)) > 0)
-        {
-            // append the block as hex
-            for (int i = 0; i < bytesRead; i++)
-            {
-                sb.append(String.format("%02X", buffer[i]));
-            }
-        }
-        stream.close();
-
-        return sb.toString();
-    }
-
-
-    //TODO - better function name, create helper functions and docblocks
+    /**
+     * Constructs a hash map of opcodes to their mnemonics and arguments.
+     *
+     * Hashmap keys are Strings of their numberic value.
+     * Values are an array consisting of [Mnemonic, Arguments] where Arguments are comma-separated.
+     *
+     * @return Map<String, String[]>
+     */
     public static Map<String, String[]> constructHashMap() {
 
-        //TODO - better variable names
         Map map = new HashMap<String, String>();
 
-
         try {
-            ClassLoader classLoader = Main.class.getClassLoader();
+            ClassLoader classLoader = Disassembler.class.getClassLoader();
+            // Get the HTML file from which to construct the hashmap
             File input = new File(classLoader.getResource("coder32_opcodes_round2.html").getFile());
-            System.out.println("got table");
 
             Document doc = Jsoup.parse(input, "UTF-8", "http://sparksandflames.com");
-
-            Element table = doc.select("table").get(0); //select the first table.
+            Element table = doc.select("table").get(0);
             Elements rows = table.select("tr");
-
-            Elements headers = rows.first().select("th");
-
 
             for (int i = 0; i < rows.size(); i++) {
                 Element row = rows.get(i);
-                Elements cols = row.select("td");
 
-                //System.out.println(row.text());
+                // Get the opcode, the mnemonic, and the arguments.
+                // Not super robust but it works
                 String opcode = rows.get(i).child(2).text();
                 String opcodeMnemonic = rows.get(i).child(10).text();
                 String arg1 = rows.get(i).child(11).text();
                 String arg2 = rows.get(i).child(12).text();
-//                System.out.println("and value for opcode is " + opcode);
-//                System.out.println("and name for opcode is " + opcodeMnemonic);
-//                System.out.println("and name for arg1 is " + arg1);
-//                System.out.println("and name for arg2 is " + arg2);
 
                 StringBuilder opcodeArgs = new StringBuilder();
                 opcodeArgs.append(arg1.trim());
                 opcodeArgs.append(",");
                 opcodeArgs.append(arg2.trim());
-
-                //System.out.println("args is " + opcodeArgs.toString());
-
 
                 String stringArray[] = new String[2];
                 stringArray[0] = opcodeMnemonic;
@@ -88,17 +52,12 @@ public class Helper {
 
                 map.put(opcode, stringArray);
 
-
-
+                // If the row we're processing has multiple entries, we need to skip at least one
                 if (row.select("td").hasAttr("rowspan")) {
-                     int rowspan = Integer.parseInt(row.select("td").attr("rowspan"));
-                    //System.out.println("rowspan 2");
-                    i+= rowspan - 1 ;
+                    int rowspan = Integer.parseInt(row.select("td").attr("rowspan"));
+                    i+= rowspan - 1;
                 }
-
-
             }
-            //TODO - do some better exception handling here
         } catch (Exception e) {
 
         }
@@ -109,6 +68,16 @@ public class Helper {
 
     }
 
+    /**
+     * Hacky function which fills in the missing values from the Opcodes table.
+     * Need to fill in 40-59 since the opcodes table only lists "40+r", "48+r" etc.
+     * If I had more time to refactor this method to make it look nicer and less clunky,
+     * I absolutely would.
+     *
+     * @param map Hashmap of opcodes.
+     *
+     * @return map new Hashmap with missing values included.
+     */
     public static Map<String, String[]> fillInMissingValues(Map<String, String[]> map) {
         String[] incs = new String[]{ "40", "41", "42", "43", "44", "45", "46", "47" };
         String[] decs = new String[]{ "48", "49" };
@@ -117,7 +86,6 @@ public class Helper {
 
         for (char i = 0; i < 256; i++) {
             if (map.get(String.format("%02x", (int)i)) != null) {
-                //System.out.println("map already has value " + String.format("%02x", (int)i));
             } else {
                 String arr[] = new String[2];
                 arr[0] = "NOP";
@@ -144,14 +112,19 @@ public class Helper {
 
 
                 map.put(String.format("%02x", (int)i).toUpperCase(), arr);
-                //System.out.println("put NOP for " + String.format("%02x", (int)i));
             }
-            //System.out.println(String.format("%04x", (int)i));
         }
         return map;
 
     }
 
+    /**
+     * Useful function to print the Hashmap. Used for debugging purposes.
+     *
+     * @param map
+     *
+     * @return void
+     */
     public static void printHashMap(Map<String, String[]> map) {
 
         for (String key: map.keySet()) {
@@ -163,6 +136,14 @@ public class Helper {
 
     }
 
+    /**
+     * Returns true if the array of strings contains the key.
+     *
+     * @param array
+     * @param key
+     *
+     * @return boolean
+     */
     private static boolean arrayContainsKey(String[] array, String key) {
         for (String i : array
              ) {
@@ -174,4 +155,40 @@ public class Helper {
         return false;
     }
 
+    /**
+     * Useful function to read the bytes of a string and print them out one at a time.
+     *
+     * @return void
+     */
+    public static void readBytes() {
+        try {
+            File file = new File("/Users/kris/Desktop/Personal/DePaul/SE526/disassembler/mybinaryfile");
+
+            InputStream inputStream = new FileInputStream(file);
+            long length = file.length();
+
+            byte[] bytes = new byte[(int) length];
+
+            inputStream.read(bytes);
+            inputStream.close();
+
+            //int numBytesToPrint = 100;
+            int start = 15776;
+            int numBytesToPrint = bytes.length - start;
+
+            for (int i = start; i < numBytesToPrint-3; i+=4) {
+                String bytesToPrint = String.format("%02x", bytes[i]) + " " +
+                        String.format("%02x", bytes[i+1]) + " " +
+                        String.format("%02x", bytes[i+2]) + " " +
+                        String.format("%02x", bytes[i+3]);
+                System.out.println(bytesToPrint);
+            }
+            String s = new String(bytes);
+            //Print the byte data into string format
+            for (int i = 0; i < s.length(); i++ ) {
+                //System.out.print(String.format("%c", s.charAt(i)));
+            }
+        } catch (Exception e) {
+        }
+    }
 }
